8b6fcd9e47d0306f3ccf8248afad2893c84d7665
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2021-02-22 22:14:22 +0000

    task(calendar): remove unused component

--- src/app/shared/calendar/calendar.module.ts
+++ src/app/shared/calendar/calendar.module.ts
@@ -1,13 +1,8 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BubbleCalendarComponent } from './components/bubble-calendar/bubble-calendar.component';
-
-
+import { NgModule } from '@angular/core';
 
 @NgModule({
-  declarations: [BubbleCalendarComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [],
+  imports: [CommonModule]
 })
-export class CalendarModule { }
+export class CalendarModule {}
